e0754e1fb2f091622244658dbb3142ca515d0be7
serhatbek <serhat.bekk@gmail.com>
2022-08-31 10:38:06 +0300

    Cocktails - About and Error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore rem
+        distinctio facere hic officia neque, natus rerum magni atque totam sequi
+        doloremque temporibus voluptatibus vitae eligendi vel possimus provident
+        perferendis officiis? Corporis dolore veniam optio at corrupti, ex eum
+        ab.
+      </p>
+    </section>
+  );
 };
 
 export default About;
