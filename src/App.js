13bde5e81171ab610630636b3017b3c0be8a4ee4
serhatbek <serhat.bekk@gmail.com>
2022-08-31 10:15:16 +0300

    router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Error from './pages/Error';
+import SingleCocktail from './pages/SingleCocktail';
+
+import Navbar from './components/Navbar';
 
 function App() {
-  return <div className='App'>app</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='cocktail/:id' element={<SingleCocktail />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
