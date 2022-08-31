0b9c1cf510ed2aa139cb2abfc5240fbf80481a8a
serhatbek <serhat.bekk@gmail.com>
2022-08-31 13:44:37 +0300

    Search Form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,8 +1,37 @@
+import { useRef, useEffect } from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
-  return <div>SearchForm</div>;
+  const searchValue = useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventdefault();
+  };
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
