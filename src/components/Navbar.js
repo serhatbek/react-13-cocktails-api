a051b0e81dc1cc3453beb8ac31dcd3d9ba3f87dc
serhatbek <serhat.bekk@gmail.com>
2022-08-31 10:29:42 +0300

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import logo from '../logo.svg';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='Cocktails DB Logo' className='logo' />
+        </Link>
+        <ul className='nav-links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
